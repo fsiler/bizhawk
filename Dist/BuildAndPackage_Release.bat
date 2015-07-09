d2fa0c92480cf3b4d5c4469d5aa596d534e35c20
zeromus <zeromus@zeromus.org>
2015-07-09 02:54:40 +0000

    delete everything from svn

--- Dist/BuildAndPackage_Release.bat
+++
@@ -1,4 +0,0 @@
-start fart "..\Version\VersionInfo.cs" "DeveloperBuild = true" "DeveloperBuild = false"
-call BuildAndPackage.bat
-start fart "..\Version\VersionInfo.cs" "DeveloperBuild = false" "DeveloperBuild = true"
-exit
